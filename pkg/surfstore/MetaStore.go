a48c50740cbf26a917a0d14ba83397b1bf0a15a5
liupeining <pel015@ucsd.edu>
2024-05-16 02:40:48 -0700

    finished metastore, haven't test

--- pkg/surfstore/MetaStore.go
+++ pkg/surfstore/MetaStore.go
@@ -2,6 +2,7 @@ package surfstore
 
 import (
 	context "context"
+	"sync"
 
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
@@ -9,19 +10,61 @@ import (
 type MetaStore struct {
 	FileMetaMap    map[string]*FileMetaData
 	BlockStoreAddr string
+	RWMutex        sync.RWMutex
 	UnimplementedMetaStoreServer
 }
 
 func (m *MetaStore) GetFileInfoMap(ctx context.Context, _ *emptypb.Empty) (*FileInfoMap, error) {
-	panic("todo")
+	// Retrieves the server's FileInfoMap
+	// map<string, FileMetaData> fileInfoMap
+	// string: used to get the member of FileInfoMap struct, I think.
+	m.RWMutex.RLock()
+	defer m.RWMutex.RUnlock()
+	//type FileInfoMap struct {
+	//	state         protoimpl.MessageState
+	//	sizeCache     protoimpl.SizeCache
+	//	unknownFields protoimpl.UnknownFields
+	//
+	//	FileInfoMap map[string]*FileMetaData `protobuf:"bytes,1,rep,name=fileInfoMap,proto3" json:"fileInfoMap,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	//}
+	fileInfoMap := &FileInfoMap{
+		FileInfoMap: make(map[string]*FileMetaData),
+	}
+	for k, v := range m.FileMetaMap {
+		fileInfoMap.FileInfoMap[k] = v
+	}
+	return fileInfoMap, nil
 }
 
 func (m *MetaStore) UpdateFile(ctx context.Context, fileMetaData *FileMetaData) (*Version, error) {
-	panic("todo")
+	m.RWMutex.RLock()
+	defer m.RWMutex.RUnlock()
+	//message FileMetaData {
+	//    string filename = 1;
+	//    int32 version = 2;
+	//    repeated string blockHashList = 3;
+	//}
+	_, exists := m.FileMetaMap[fileMetaData.Filename]
+	if exists {
+		curVersion := m.FileMetaMap[fileMetaData.Filename].Version
+		if fileMetaData.Version > curVersion {
+			m.FileMetaMap[fileMetaData.Filename] = fileMetaData
+		} else {
+			return &Version{Version: -1}, nil
+		}
+	} else {
+		m.FileMetaMap[fileMetaData.Filename] = fileMetaData
+	}
+	return &Version{Version: fileMetaData.Version}, nil
 }
 
 func (m *MetaStore) GetBlockStoreAddr(ctx context.Context, _ *emptypb.Empty) (*BlockStoreAddr, error) {
-	panic("todo")
+	//message BlockStoreAddr {
+	//    string addr = 1;
+	//}
+	m.RWMutex.RLock()
+	defer m.RWMutex.RUnlock()
+	return &BlockStoreAddr{Addr: m.BlockStoreAddr}, nil
 }
 
 // This line guarantees all method for MetaStore are implemented
